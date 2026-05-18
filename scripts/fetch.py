b49468ac2ec9b85ddcb826d89316f7384c1d78de
mediumsizedcar <50729802+mediumsizedcar@users.noreply.github.com>
2026-05-18 14:29:29 -0500

    scripts/fetch.py: Lazy attempt

--- scripts/fetch.py
+++ scripts/fetch.py
@@ -13,11 +13,11 @@ NOW = datetime.datetime.now(datetime.UTC)
 TIMESTAMP = NOW.timestamp() * 1000
 CWD = Path(__file__).parent
 
-def dump(users: list[dict[str, str]], game: str, playtype: str) -> None:
+def dump(users: list[dict[str, str]], game: str) -> None:
 	userdata = {"lastUpdate": TIMESTAMP, "profiles": []}
 	for user in users:
 		user_id = user["id"]
-		url = BASE_URL + API_ROUTE + f"/users/{user_id}/games/{game}/{playtype}"
+		url = BASE_URL + API_ROUTE + f"/users/{user_id}/games/{game}"
 		print(url)
 
 		r = requests.get(url).json()
@@ -34,9 +34,9 @@ def dump(users: list[dict[str, str]], game: str, playtype: str) -> None:
 		userdata["profiles"].append(profile)
 
 	if userdata:
-		userdata_dir_path = CWD.parent / "userdata" / game
+		userdata_dir_path = CWD.parent / "userdata"
 		userdata_dir_path.mkdir(parents=True, exist_ok=True)
-		userdata_file_path = userdata_dir_path / f"{playtype}.json"
+		userdata_file_path = userdata_dir_path / f"{game}.json"
 		with open(userdata_file_path, mode="w", encoding="utf-8") as userdata_file:
 			json.dump(userdata, userdata_file, indent="\t")
 		print(f"\t{userdata_file_path}")
@@ -45,28 +45,28 @@ def main() -> None:
 	r = requests.get(BASE_URL + API_ROUTE + "/games").json()
 	if not r["success"]:
 		return
-	gpts = [(game, playtype)
-		 for game in r["body"]["supportedGames"]
-		 for playtype in r["body"]["configs"][game]["playtypes"]]
+	games = [game
+		  for config in r["body"]["configs"].values()
+		  for game in config["games"]]
 
 	i = (NOW.hour % 2 * 60 + NOW.minute) // 5
 	dump_all = len(sys.argv) > 1 and sys.argv[1].lower() == "all"
 
 	if dump_all:
 		pass
-	elif i >= len(gpts):
+	elif i >= len(games):
 		return
 	else:
-		(game, playtype) = gpts[i]
+		game = games[i]
 
 	with open(CWD / "users.json", mode="r", encoding="utf-8") as users_file:
 		users = json.load(users_file)
 	
 	if dump_all:
-		for g, pt in gpts:
-			dump(users, g, pt)
+		for g in games:
+			dump(users, g)
 	else:
-		dump(users, game, playtype)
+		dump(users, game)
 
 if (__name__ == "__main__"):
 	main()
